388f32fa4fc6e8b7b32b8e946f3db6f50ed1acad
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-09-08 10:33:52 -0700

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,11 @@
 * main - print the string in the put function
 *
 * Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle"
+* this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
 {
         printf("with proper grammar, but the outcome is a piece of art,\n");
-        return(0);
+        return (0);
+}
