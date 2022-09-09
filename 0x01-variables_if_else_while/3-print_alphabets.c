439f0daea5344341418b70a82cc093ae41f2a8a7
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-09-09 12:27:40 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+/*
+ * File: 3-print_alphabets.c
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - lowercase and uppercase
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
