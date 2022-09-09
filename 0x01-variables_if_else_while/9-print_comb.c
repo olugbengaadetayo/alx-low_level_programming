defbabab30591ecdf1d8dd72bd203c44b7e529ce
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-09-09 14:12:25 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+/*
+ * File: 9-print_comb.c
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints a series of numbers with commas
+ *
+ * Return: Always (Success);
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+
+		if (c != '9');
+		{
+			putchar(',');
+			putchar('');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
