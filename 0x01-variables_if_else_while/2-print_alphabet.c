dbeaa310a0bfd225fead00ead53aadd3bd1b9829
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-09-09 12:10:47 -0700

    task3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-print_alphabet.c
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - lowercase alphabets
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
