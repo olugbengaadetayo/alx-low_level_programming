d56e78e040151b269dc3f66ca3b1706cea093d51
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-09-09 13:26:45 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+/**
+ * File: 8-print_base16.c
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints hexadecimal string
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
