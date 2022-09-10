882ce7fd17da2b22fb84a0e312dab00a5da0eef5
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-09-09 18:53:28 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,11 +14,11 @@ int main(void)
 {
 	int c;
 
-	for (c = '48'; c <= '57'; c++)
+	for (c = 48; c <= 57; c++)
 	{
 		putchar(c);
 
-		if (c != '57');
+		if (c != 57)
 		{
 			putchar(',');
 			putchar(' ');
