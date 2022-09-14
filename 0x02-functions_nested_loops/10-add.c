47091a9566d6f0e1eac8ef45da19f2afdc653ed0
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-09-14 12:21:20 -0700

    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,7 +5,7 @@
  * @num1: the first integer to be added
  * @num2: the second integer to be added
  *
- * Return: The result of the addition 
+ * Return: The result of the addition
  */
 int add(int num1, int num2)
 {
