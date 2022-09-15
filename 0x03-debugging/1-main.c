84b3c2dfb0118a4b83c08c02f9d7179ebc11c152
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-09-15 14:48:16 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n"));
 
 	i = 0;
 
@@ -21,4 +21,5 @@ int main(void)
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
+
 }
