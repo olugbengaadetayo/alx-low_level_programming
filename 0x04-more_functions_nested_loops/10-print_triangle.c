4df064b46ef6d372d79304a83391ed95851216cf
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-09-16 06:37:14 -0700

    task 12

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size parameter of triangle
+ * Return: returns nothing
+ */
+
+void print_triangle(int size)
+{
+	int inc1, inc2;
+
+	if (size 
