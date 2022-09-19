b707910ced2df2a707fb2e5ab433839aac9fcace
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-09-19 10:42:50 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print rev - printing a string in reverse
+ * @s: the string to be printed in rev
+ */
+
+void rev_string(char *s)
+{
+	int i, n;
+	s = "hello"
+	n = strlen(s);
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
