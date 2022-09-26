81bdfd04f8fe272251f5b2c930329d7ebdc2ee2e
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-09-26 05:32:12 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -20,7 +20,7 @@ void print_diagsums(int *a, int size)
 	}
 	for (j = 0; j < size; j++)
 	{
-		p = (j * size) + (size -i - j);
+		p = (j * size) + (size - i - j);
 		r += *(a + p);
 	}
 	printf("%i, %i\n", l, r);
