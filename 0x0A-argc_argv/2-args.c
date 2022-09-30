43884523c52525e79c7dedfe4e088674e51f0acb
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-09-30 05:29:07 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that prints its name
+ * @argc: counts arguments
+ * @argv: an array of command listed
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for i = 0, i < argc, i++)
+	{
+		printf("%s\n", i, argv[i]);
+	}
+	return (0);
+}
