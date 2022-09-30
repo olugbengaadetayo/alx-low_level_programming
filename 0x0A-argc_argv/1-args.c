96ef8e02777c294fe2142ef4ba24ff7c5a653520
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-09-30 05:08:11 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: counts arguments
+ * @argv: argument
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%i\n", argc -1)
+
+return (0);
+}
