83d74d03f448b8b23d8db2171b6110b9982396e7
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-09-30 08:24:46 -0700

    task 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	if(num < 0)
 	{
 		printf("0\n");
-		return ()0;
+		return (0);
 	}
 	for (j = 0; j < 5 && num >= 0; j++)
 	{
@@ -37,4 +37,3 @@ int main(int argc, char *argv[])
 	printf("%d\n", result);
 	return (0);
 }
-
