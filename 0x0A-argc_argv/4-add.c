a4e2fa181d2e4b4545cf2de8c620c380c1c4df7b
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-09-30 07:59:23 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,16 +13,15 @@ int main(int argc, char *argv[])
 {
 	int result = 0, num, i, j, k;
 
-	if (arg == 1)
-		printf("0\n")
-
 	for (i = 0; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("%s\n", argv[i]);
-			printf("%s\n", "Error");
-			return (1);
+			if (argv[i][j] > '9' || argv[i][j] < '\0')
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
 		}
 	}
 
