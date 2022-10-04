1c6007c0e52ae0ff8bba14c13d98d985e7b99b32
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-10-04 08:58:29 -0700

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -60,7 +60,7 @@ char **strtow(char *str)
 
 	if (words == 0)
 		return (NULL);
-	split = (char **)malloc(sizeof(char *) * (words + 1));
+	split = (char **) malloc(sizeof(char *) * (words + 1));
 	if (split != NULL)
 	{
 		for (i = 0; i <= len(str) && words; i++)
@@ -69,7 +69,7 @@ char **strtow(char *str)
 				size++;
 			else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
 			{
-				split[j] = (char *)malloc(sizeof(char) * size + 1);
+				split[j] = (char *) malloc(sizeof(char) * size + 1);
 				if (split[j] != NULL)
 				{
 					while (temp < size)
