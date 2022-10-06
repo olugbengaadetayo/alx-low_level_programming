9f45405cf1f6bf4b7719f651098ef2bb7b2c6249
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-10-06 16:28:03 -0700

    task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,7 +10,7 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *clone, relloc;
+	char *clone, realloc;
 	unsigned int i;
 
 	if (ptr != NULL)
@@ -20,14 +20,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 	return (ptr);
 	if (new_size == 0 && ptr != NULL)
-	{ free(ptr);
+	{ free (ptr);
 	return (0); }
 	relloc = malloc(new_size);
 	if (relloc == NULL)
 	return (0);
 	for (i = 0; i < (old_size || i < new_size); i++)
 	{
-		*(relloc + i) - clone[i];
+		*(relloc + i) = clone[i];
 	}
 	free(ptr);
 return (relloc);
