9c8f802a0d6503b1ce747ac50f8c15283c5b007f
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-10-06 13:24:18 -0700

    task 0

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,4 +12,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
-#end if /*MAIN_H*/
+#endif /*MAIN_H*/
