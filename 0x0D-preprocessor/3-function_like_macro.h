cd39cb74f8d99ac13923bb043b943a5f645c04cb
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-10-10 06:59:02 -0700

    task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef ABSO
-#define ABSO
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) (((X) < (0)) ? (-(X)) : (X))
+#define ABS(x) ((x < 0) ? (x) * (-1) : (x))
 
 #endif
