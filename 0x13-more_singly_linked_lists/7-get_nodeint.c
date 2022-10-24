7b53dad22d054e0f8215f35c10cf8656f451aea8
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-10-24 02:46:25 -0700

    task 8

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,14 +12,14 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int node;
-	
+
 	for (node = 0; node < index; node++)
 	{
 		if (head == NULL)
 			return (NULL);
-		
+
 		head = head->next;
 	}
-	
+
 	return (head);
 }
