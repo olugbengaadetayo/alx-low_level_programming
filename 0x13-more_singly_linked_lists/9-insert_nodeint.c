42cbf7d8a9d8b76f1933076ac4d8361d761797db
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-10-24 03:11:17 -0700

    task 10

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	for (node = 0; node < (idx - 1); node++)					{
+	for (node = 0; node < (idx - 1); node++)
+	{
 		if (copy == NULL || copy->next == NULL)
 			return (NULL);
 
