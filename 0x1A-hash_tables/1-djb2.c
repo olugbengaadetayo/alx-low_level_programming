4dcbb2ae24bd312dd86a3f30a87b8554b9c701ef
olugbenga adetayo <olugbengaadetayo@gmail.com>
2023-01-06 03:24:28 -0800

    task 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -14,6 +14,6 @@ unsigned long int hash_djb2(const unsigned char *str)
 	hash = 5381;
 	while ((count = *str++))
 		hash = ((hash << 5) + hash) + count; /* hash * 33 + count */
-	
+
 	return (hash);
 }
